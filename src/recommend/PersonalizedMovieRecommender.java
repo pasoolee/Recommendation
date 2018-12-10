d760bd017269c54e2ec47efe53cfe3aa8eafe78f
Yongchao Li <pasoolee@outlook.com>
2018-12-10 09:24:05 -0500

    1 change format for popularMovieRecommender and PersonalizedMovieRecommender

--- src/recommend/PersonalizedMovieRecommender.java
+++ src/recommend/PersonalizedMovieRecommender.java
@@ -14,141 +14,115 @@ public class PersonalizedMovieRecommender implements IRecommender<Movie> {
 	private TreeMap<Movie, String> _sortMovieMap;
 	private popularMovieRecommender popMovieRec;
 
-
 	@Override
-	public Movie[] recommend(int userId, int n)
-	{
-		String SimUserId = mostSimUser(userId + "");		
-		Movie[] recomendMovies = popMovieRec.recommend(Integer.parseInt(SimUserId), n);		
+	public Movie[] recommend(int userId, int n) {
+		String SimUserId = mostSimUser(userId + "");
+		Movie[] recomendMovies = popMovieRec.recommend(Integer.parseInt(SimUserId), n);
 		return recomendMovies;
 	}
 
-	public Movie[] recommend(int userId, int n,String genre)
-	{
-		String SimUserId = mostSimUser(userId + "");		
-		Movie[] recomendMovies = popMovieRec.recommend(Integer.parseInt(SimUserId), n, genre);		
+	public Movie[] recommend(int userId, int n, String genre) {
+		String SimUserId = mostSimUser(userId + "");
+		Movie[] recomendMovies = popMovieRec.recommend(Integer.parseInt(SimUserId), n, genre);
 		return recomendMovies;
 	}
-	public PersonalizedMovieRecommender(Movie[] movies, Rating[] ratings)
-	{
+
+	public PersonalizedMovieRecommender(Movie[] movies, Rating[] ratings) {
 		InitMovie(movies);
 		InitRating(ratings);
 		SortMovies();
 		popMovieRec = new popularMovieRecommender(movies, ratings);
 	}
 
-
 	private void SortMovies() {
 		_sortMovieMap = new TreeMap<Movie, String>();
-		
-		for (String key : _movieMap.keySet())
-		{
+
+		for (String key : _movieMap.keySet()) {
 			_sortMovieMap.put(_movieMap.get(key), key);
 		}
-		
-	}
 
+	}
 
 	private void InitRating(Rating[] ratings) {
 		_ratingMap = new HashMap<String, HashMap<String, String>>();
-		
-		for(int i = 0; i< ratings.length; i++)
-		{
-			HashMap<String, String> movieRating;			
-			
-			if(!_movieMap.containsKey(ratings[i].getItemId()))
-			{
+
+		for (int i = 0; i < ratings.length; i++) {
+			HashMap<String, String> movieRating;
+
+			if (!_movieMap.containsKey(ratings[i].getItemId())) {
 				continue;
 			}
-			
-			Movie putRateValueMovie = _movieMap.get(ratings[i].getItemId());				
-			
+
+			Movie putRateValueMovie = _movieMap.get(ratings[i].getItemId());
+
 			putRateValueMovie.AddRate(ratings[i].getName(), ratings[i].getRateValue());
-			
-			if (!_ratingMap.containsKey(ratings[i].getName()))
-			{
-				movieRating = new HashMap<String, String>();				
+
+			if (!_ratingMap.containsKey(ratings[i].getName())) {
+				movieRating = new HashMap<String, String>();
 				movieRating.put(ratings[i].getItemId(), ratings[i].getRateValue());
 				_ratingMap.put(ratings[i].getName(), movieRating);
-			}
-			else
-			{
+			} else {
 				movieRating = _ratingMap.get(ratings[i].getName());
-				
-				if (!movieRating.containsKey(ratings[i].getItemId()))
-				{
+
+				if (!movieRating.containsKey(ratings[i].getItemId())) {
 					movieRating.put(ratings[i].getItemId(), ratings[i].getRateValue());
 				}
 			}
-		}		
+		}
 
-		
 	}
-
 
 	private void InitMovie(Movie[] movies) {
 		_movieMap = new HashMap<String, Movie>();
 
-		for(int i = 0; i < movies.length; i++)
-		{
-			if (!_movieMap.containsKey(movies[i].getItemId()))
-			{
+		for (int i = 0; i < movies.length; i++) {
+			if (!_movieMap.containsKey(movies[i].getItemId())) {
 				_movieMap.put(movies[i].getItemId(), movies[i]);
-			}
-			else
-			{
+			} else {
 				continue;
 			}
-		}		
+		}
 	}
-	
-	private String mostSimUser(String userid)
-	{
+
+	private String mostSimUser(String userid) {
 		String mostSimUserId = null;
-		
+
 		double SimMax = 0;
-		
-		HashMap<String, String> userMovieRating  = _ratingMap.get(userid);		
-		
-		for(String comparingUserId : _ratingMap.keySet())
-		{
-			for(String userMovieId : userMovieRating.keySet())
-			{
+
+		HashMap<String, String> userMovieRating = _ratingMap.get(userid);
+
+		for (String comparingUserId : _ratingMap.keySet()) {
+			for (String userMovieId : userMovieRating.keySet()) {
 				double accumulating = 0;
-				
-				for(String compareUserMovieId : _ratingMap.get(comparingUserId).keySet())
-				{
-					if (userMovieId.equals(compareUserMovieId))
-					{
+
+				for (String compareUserMovieId : _ratingMap.get(comparingUserId).keySet()) {
+					if (userMovieId.equals(compareUserMovieId)) {
 						double userRating = Double.parseDouble(userMovieRating.get(userMovieId));
-						double SimUserRating = Double.parseDouble(_ratingMap.get(comparingUserId).get(compareUserMovieId));
-						accumulating += (userRating - 2.5)*(SimUserRating - 2.5);
+						double SimUserRating = Double
+								.parseDouble(_ratingMap.get(comparingUserId).get(compareUserMovieId));
+						accumulating += (userRating - 2.5) * (SimUserRating - 2.5);
 					}
 				}
-				
-				if(accumulating > SimMax)
-				{
+
+				if (accumulating > SimMax) {
 					SimMax = accumulating;
 					mostSimUserId = comparingUserId;
 				}
 			}
 		}
-		
+
 		return mostSimUserId;
 	}
-	
-	public boolean hasUserId(String userid)
-	{
+
+	public boolean hasUserId(String userid) {
 		return popMovieRec.hasUserId(userid);
 	}
-	
-	public void Test()
-	{
-		for(String comparingUserId : _ratingMap.keySet())
-		{
+
+	public void Test() {
+		for (String comparingUserId : _ratingMap.keySet()) {
 			System.out.println(comparingUserId + " - " + mostSimUser(comparingUserId));
 		}
-		
+
 	}
-	
+
 }
--- src/recommend/popularMovieRecommender.java
+++ src/recommend/popularMovieRecommender.java
@@ -13,31 +13,27 @@ public class popularMovieRecommender implements IRecommender<Movie> {
 
 	private HashMap<String, Movie> _movieMap;
 	private HashMap<String, HashMap<String, String>> _ratingMap;
-	private TreeMap<Movie, String> _sortMovieMap;	
-	
+	private TreeMap<Movie, String> _sortMovieMap;
+
 	@Override
 	public Movie[] recommend(int userId, int n) {
 		String userIdStr = userId + "";
 		ArrayList<Movie> movieRecommend = new ArrayList<Movie>();
-	
-		for (Map.Entry<Movie, String> entry : _sortMovieMap.entrySet())
-		{
+
+		for (Map.Entry<Movie, String> entry : _sortMovieMap.entrySet()) {
 			Movie movie = entry.getKey();
-			if (!movie.isRatedByUser(userIdStr))
-			{
+			if (!movie.isRatedByUser(userIdStr)) {
 				movieRecommend.add(movie);
 				n--;
 			}
-			if (n == 0)
-			{
+			if (n == 0) {
 				break;
 			}
 		}
-		
+
 		Movie[] movieRecommendArray = new Movie[movieRecommend.size()];
-		
-		for (int i = 0; i < movieRecommendArray.length; i++)
-		{
+
+		for (int i = 0; i < movieRecommendArray.length; i++) {
 			try {
 				movieRecommendArray[i] = movieRecommend.get(i).deepCopy();
 			} catch (Exception e) {
@@ -45,52 +41,43 @@ public class popularMovieRecommender implements IRecommender<Movie> {
 				e.printStackTrace();
 			}
 		}
-		
+
 		return movieRecommendArray;
 	}
-	
-	public boolean hasUserId(String userid)
-	{
+
+	public boolean hasUserId(String userid) {
 		boolean hasUser = false;
-		
-		for(String index : _ratingMap.keySet())
-		{
-			if (index.equals(userid))
-			{
+
+		for (String index : _ratingMap.keySet()) {
+			if (index.equals(userid)) {
 				hasUser = true;
 				break;
-			}
-			else
-			{
+			} else {
 				continue;
 			}
-		}	
-		
+		}
+
 		return hasUser;
 	}
 
 	public Movie[] recommend(int userId, int n, String genre) {
 		String userIdStr = userId + "";
 		ArrayList<Movie> movieRecommend = new ArrayList<Movie>();
-	
-		for (Map.Entry<Movie, String> entry : _sortMovieMap.entrySet())
-		{
+
+		for (Map.Entry<Movie, String> entry : _sortMovieMap.entrySet()) {
 			Movie movie = entry.getKey();
-			if (!movie.isRatedByUser(userIdStr) && movie.hasGenre(genre))
-			{
+			if (!movie.isRatedByUser(userIdStr) && movie.hasGenre(genre)) {
 				movieRecommend.add(movie);
 				n--;
 			}
-			if (n == 0)
-			{
+			if (n == 0) {
 				break;
 			}
 		}
-		
+
 		Movie[] movieRecommendArray = new Movie[movieRecommend.size()];
-		
-		for (int i = 0; i < movieRecommendArray.length; i++)
-		{
+
+		for (int i = 0; i < movieRecommendArray.length; i++) {
 			try {
 				movieRecommendArray[i] = movieRecommend.get(i).deepCopy();
 			} catch (Exception e) {
@@ -98,24 +85,21 @@ public class popularMovieRecommender implements IRecommender<Movie> {
 				e.printStackTrace();
 			}
 		}
-		
+
 		return movieRecommendArray;
 	}
-	
-	
-	public popularMovieRecommender(Movie[] movies, Rating[] ratings)
-	{		
+
+	public popularMovieRecommender(Movie[] movies, Rating[] ratings) {
 		InitMovie(movies);
-		InitRating(ratings);		
+		InitRating(ratings);
 		SortMovies();
 	}
 
 	private void SortMovies() {
-		
+
 		_sortMovieMap = new TreeMap<Movie, String>();
-		
-		for (String key : _movieMap.keySet())
-		{
+
+		for (String key : _movieMap.keySet()) {
 			_sortMovieMap.put(_movieMap.get(key), key);
 		}
 
@@ -125,63 +109,50 @@ public class popularMovieRecommender implements IRecommender<Movie> {
 
 		_movieMap = new HashMap<String, Movie>();
 
-		for(int i = 0; i < movies.length; i++)
-		{
-			if (!_movieMap.containsKey(movies[i].getItemId()))
-			{
+		for (int i = 0; i < movies.length; i++) {
+			if (!_movieMap.containsKey(movies[i].getItemId())) {
 				_movieMap.put(movies[i].getItemId(), movies[i]);
-			}
-			else
-			{
+			} else {
 				continue;
 			}
 		}
-		
+
 	}
-	
+
 	private void InitRating(Rating[] ratings) {
 
 		_ratingMap = new HashMap<String, HashMap<String, String>>();
-		
-		for(int i = 0; i< ratings.length; i++)
-		{
+
+		for (int i = 0; i < ratings.length; i++) {
 			HashMap<String, String> movieRating;
-			
-			if(!_movieMap.containsKey(ratings[i].getItemId()))
-			{
+
+			if (!_movieMap.containsKey(ratings[i].getItemId())) {
 				continue;
 			}
-			
+
 			Movie putRateValueMovie = _movieMap.get(ratings[i].getItemId());
-						
+
 			putRateValueMovie.AddRate(ratings[i].getName(), ratings[i].getRateValue());
-			
-			if (!_ratingMap.containsKey(ratings[i].getName()))
-			{
-				movieRating = new HashMap<String, String>();				
+
+			if (!_ratingMap.containsKey(ratings[i].getName())) {
+				movieRating = new HashMap<String, String>();
 				movieRating.put(ratings[i].getItemId(), ratings[i].getRateValue());
 				_ratingMap.put(ratings[i].getName(), movieRating);
-			}
-			else
-			{
+			} else {
 				movieRating = _ratingMap.get(ratings[i].getName());
-				
-				if (!movieRating.containsKey(ratings[i].getItemId()))
-				{
+
+				if (!movieRating.containsKey(ratings[i].getItemId())) {
 					movieRating.put(ratings[i].getItemId(), ratings[i].getRateValue());
 				}
 			}
 		}
 	}
-	
-	public void Test()
-	{
+
+	public void Test() {
 		int i = 0;
-		for (Movie key : _sortMovieMap.keySet())
-		{
+		for (Movie key : _sortMovieMap.keySet()) {
 			System.out.println(key.getItemId() + " " + key.getAvgRating());
-			if (++i == 50)
-			{
+			if (++i == 50) {
 				break;
 			}
 		}
